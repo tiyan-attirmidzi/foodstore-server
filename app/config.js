0ee2282ce6fc3634c5c53eb43a733a848d2e7704
tiyan-attirmidzi <tiyanattirmidzi20@gmail.com>
2021-04-25 23:59:35 +0800

    configurate database

--- app/config.js
+++ app/config.js
@@ -1,3 +1,12 @@
 const dotenv = require('dotenv')
 
 dotenv.config()
+
+module.exports = {
+    serviceName: process.env.SERVICE_NAME,
+    dbHost: process.env.DB_HOST,
+    dbUser: process.env.DB_USER,
+    dbPass: process.env.DB_PASSWORD,
+    dbPort: process.env.DB_PORT,
+    dbName: process.env.DB_NAME,
+}
