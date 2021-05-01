f83ba9f9992eea9c4f50dcc5511c00e0cb27173a
tiyan-attirmidzi <tiyanattirmidzi20@gmail.com>
2021-05-01 23:32:38 +0800

    add filtering by keyword

--- app/products/controller.js
+++ app/products/controller.js
@@ -7,8 +7,17 @@ const Tag = require('../tags/model')
 
 async function index (req, res, next) {
     try {
-        let { limit = 10, skip = 0 } = req.query
-        let products = await Product.find()
+        let { limit = 10, skip = 0, q = '' } = req.query
+        let criteria = {};
+
+        if (q.query) {
+            criteria = {
+                ...criteria,
+                name: { $regex: `${q}`, $options: 'i' }
+            }
+        }
+
+        let products = await Product.find(criteria)
             .limit(parseInt(limit))
             .skip(parseInt(skip))
             .populate('category')
